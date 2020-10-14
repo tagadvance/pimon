fdeeb651a0de6db1f771968af43d36d6554374d0
Tag <tagadvance+github.com@gmail.com>
2020-10-13 20:55:35 -0600

    Refactor autoload

--- pimon.php
+++ pimon.php
@@ -4,4 +4,5 @@
 declare(strict_types=1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/vendor/autoload.php';
+$autoload = implode(DIRECTORY_SEPARATOR, [__DIR__, 'vendor', 'autoload.php']);
+require_once $autoload;
